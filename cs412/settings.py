4439d42ce976f166b2a8ee96cbf59c3a0ee0a55f
Berk Komurcuoglu <berkkom@bu.edu>
2026-01-29 15:13:45 -0500

    Changes for deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -121,3 +121,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "media/"  # note: no leading slash!
+
+import socket
+CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
+
+if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
+    STATIC_URL = '/username/static/'
+    MEDIA_URL = '/username/media/'
