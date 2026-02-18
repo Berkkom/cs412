e2debcbf9643aa7a7c8d91e1ef4d2eb2833cfe9c
Berk Komurcuoglu <berkkom@bu.edu>
2026-02-17 21:41:48 -0500

    App displaying all profiles

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,6 +1,8 @@
 # File: urls.py
 # Author: Berk Komurcuoglu (berkkom@bu.edu), 2/17/2026
+from django.urls import path
+from .views import ProfileListView
 
 urlpatterns = [
-    
+    path('', ProfileListView.as_view(),name='show_all_profiles')
 ]
